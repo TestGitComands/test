099c56a94ae4ab5270896e2f834ee056bf936b73
TestGitComands <132548236+TestGitComands@users.noreply.github.com>
2023-05-23 19:01:27 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -17,6 +17,10 @@ public class Animal {
     public Integer getAge() {
         return age;
     }
+    
+    public void setAge(int age){
+        this.age = age;   
+    }
 
     public Animal setDesc(String desc) {
         this.desc = desc;
