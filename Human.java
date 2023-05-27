5a2fac34d38335695ac0443743bfc872a5590590
TestGitComands <132548236+TestGitComands@users.noreply.github.com>
2023-05-27 13:34:38 +0300

    Update Human.java

--- Human.java
+++ Human.java
@@ -3,8 +3,9 @@ public class Human {
     private String name;
     private int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public int getAge() {
