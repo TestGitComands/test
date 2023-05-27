ea15bd09b78af75eaa159a52659f88fc6c6597fd
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2023-05-27 13:47:18 +0300

    added emtry constructor in Human

--- Human.java
+++ Human.java
@@ -3,6 +3,10 @@ public class Human {
     private String name;
     private int age;
 
+
+    public Human() {
+    }
+
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
